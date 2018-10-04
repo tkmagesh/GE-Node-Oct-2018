6468bda2f7b746b113d618c9a54f000727e24c63
Magesh <tkmagesh77@gmail.com>
2018-10-04 15:15:01 +0530

    web server implementation

--- 05-Web-Server/server.js
+++ 05-Web-Server/server.js
@@ -1,8 +1,19 @@
-const http = require('http');
+const http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
+	url = require('url');
 
 let server = http.createServer(function(req /*Readable Stream*/, res /* Writable Stream*/){
-	res.write('<h1>Welcome to Node.js</h1>');
-	res.end();
+	var urlObj = url.parse(req.url);
+	console.log(req.method + '\t' + urlObj.pathname);
+	let resource = path.join(__dirname, (urlObj.pathname === '/' ? 'index.html' : urlObj.pathname));
+	if (!fs.existsSync(resource)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	let stream = fs.createReadStream(resource);
+	stream.pipe(res);
 });
 
 server.listen(8080);
