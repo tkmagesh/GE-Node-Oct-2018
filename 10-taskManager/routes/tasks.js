3489302389678f699bd1392b429ecb3e667baf76
Magesh <tkmagesh77@gmail.com>
2018-10-05 15:08:04 +0530

    Task routes with in memory data implementation

--- 10-taskManager/routes/tasks.js
+++ 10-taskManager/routes/tasks.js
@@ -10,4 +10,13 @@ router.get('/', function(req, res, next){
 	res.json(taskList);
 });
 
+router.post('/', function(req, res, next){
+	var newTaskData = req.body;
+	newTaskData.id = taskList.reduce(function(result, task){
+		return result > task.id ? result : task.id;
+	}, 0) + 1;
+	taskList.push(newTaskData);
+	res.status(201).json(newTaskData);
+});
+
 module.exports = router;
