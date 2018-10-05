7f55dffdea4a851aba98c937447158c0e3c52cab
Magesh <tkmagesh77@gmail.com>
2018-10-05 15:28:08 +0530

    task api implementation - part - 1

--- 10-taskManager/routes/tasks.js
+++ 10-taskManager/routes/tasks.js
@@ -10,6 +10,18 @@ router.get('/', function(req, res, next){
 	res.json(taskList);
 });
 
+router.get('/:id', function(req, res, next){
+	var taskIdToFind = parseInt(req.params.id);
+	var resultTask = taskList.find(function(task){
+		return task.id === taskIdToFind;
+	});
+	if (!resultTask){
+		res.status(404);
+		return;
+	}
+	res.json(resultTask);
+});
+
 router.post('/', function(req, res, next){
 	var newTaskData = req.body;
 	newTaskData.id = taskList.reduce(function(result, task){
@@ -18,5 +30,46 @@ router.post('/', function(req, res, next){
 	taskList.push(newTaskData);
 	res.status(201).json(newTaskData);
 });
+
+router.put('/:id', function(req, res, next){
+	var taskIdToUpdate = parseInt(req.params.id),
+		updatedTask = req.body;
+
+	var taskToReplace = taskList.find(function(task){
+		return task.id === taskIdToUpdate;
+	});
+
+	if (!taskToReplace){
+		res.status(404);
+		return;
+	}
+	taskList = taskList.map(function(task){
+		return task.id === taskIdToUpdate ? updatedTask : task;
+	});
+
+	res.json(updatedTask);
+});
+
+router.delete('/:id', function(req, res, next){
+	var taskIdToDelete = parseInt(req.params.id);
+
+	var taskToDelete = taskList.find(function(task){
+		return task.id === taskIdToDelete;
+	});
+
+	if (!taskToDelete){
+		res.status(404);
+		return;
+	}
+	taskList = taskList.filter(function(task){
+		return task.id !== taskIdToDelete;
+	});
+
+	res.json({});
+});
+
+
+
+
 
 module.exports = router;
