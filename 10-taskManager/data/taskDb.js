e76b8615b80ebec2bdbcbd6f2302355ea58ebf32
Magesh <tkmagesh77@gmail.com>
2018-10-06 11:06:47 +0530

    using utils & bluebird to create promise wrappers for callback based functions

--- 10-taskManager/data/taskDb.js
+++ 10-taskManager/data/taskDb.js
@@ -3,7 +3,7 @@ const fs = require('fs'),
 
 const dbFile = path.join(__dirname, 'taskDb.json');
 
-function getAll(){
+/*function getAll(){
 	return new Promise(function(resolveFn, rejectFn){
 		fs.readFile(dbFile, { encoding : 'utf8'}, function(err, fileContents){
 			if (!err){
@@ -27,6 +27,44 @@ function save(data){
 		});	
 	})
 	
+}*/
+
+/*
+//using native util module to create promise wrappers for callback based functions
+var util = require('util');
+
+const readFile = util.promisify(fs.readFile),
+	writeFile = util.promisify(fs.writeFile);
+
+function getAll(){
+	
+	return readFile(dbFile, {encoding : 'utf8'})
+			.then(function(fileContents){
+				return JSON.parse(fileContents);
+			});
+	
+}
+
+function save(data){
+	return writeFile(dbFile, JSON.stringify(data), 'utf8');
+}*/
+
+//using bluebird
+
+var Promise = require('bluebird');
+Promise.promisifyAll(fs);
+
+function getAll(){
+	
+	return fs.readFileAsync(dbFile, {encoding : 'utf8'})
+			.then(function(fileContents){
+				return JSON.parse(fileContents);
+			});
+	
+}
+
+function save(data){
+	return fs.writeFileAsync(dbFile, JSON.stringify(data), 'utf8');
 }
 
 module.exports = {
